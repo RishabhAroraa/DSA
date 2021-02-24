8626fd81d5326dc7a92b5d18d0f7c75bc68f482f
RishabhAroraa <rishabharora.ns@gmail.com>
2021-02-24 12:08:23 +0530

    Create queue.cpp

---
+++ data-structures/queue/src/queue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class queue {
+	private:
+
+	int front;
+	int rear;
+	int* arr;
+
+	queue(int size) {
+		arr = (int*)malloc(size * sizeof(int));
+	}
+
+	void enqueue(int num) {
+		//insert code here
+	}
+}
